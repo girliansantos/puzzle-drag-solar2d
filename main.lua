a387c3142929836c6ebb718c978b727624a8f0cf
Girlian Santos <girliansantos@si.fiponline.edu.br>
2022-03-23 08:47:46 -0300

    iniciando projeto de puzzle com Corona SDK - Solar2d - baseado em Lua

---
+++ main.lua
@@ -0,0 +1,11 @@
+-----------------------------------------------------------------------------------------
+--
+-- main.lua
+--
+-----------------------------------------------------------------------------------------
+
+-- Your code here
+
+local background = display.newRect(0, 0, display.contentWidth, display.contentHeight)
+background.x = display.contentCenterX
+background.y = display.contentCenterY
